4cdc1cdbd3ef88e395484e8ce3fe07a7b7c25ec7
mouse <mouse.janpa@gmail.com>
2024-06-22 20:51:26 +0900

    make integer compiler

---
+++ mycc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Invalid arguments\n");
+    }
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("\tmov rax, %d\n", atoi(argv[1]));
+    printf("\tret\n");
+    return 0;
+}
